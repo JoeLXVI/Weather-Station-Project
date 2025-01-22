cb1199fb20cf622d300f10481ebef2120b030b2c
JoeLXVI <joe.matthew.lucas@gmail.com>
2025-01-22 14:57:18 +0000

    Add Comments

--- WeatherStation_LightSensor_V1.py
+++ WeatherStation_LightSensor_V1.py
@@ -6,29 +6,37 @@ Last Modified:
 Description: This code defines a class for a light sensor including attributes and methods to allow for the reading and display of sensor data.
 """
 
+# Import Required Modules
 import time
 import machine
 
 
-class LightSensor:  #
+# Define Class
+class LightSensor:
+
+    # Define Constants
     ConversionFactor = 0.0576
     CommandRegister = 0x00
     HighByte = 0x04
     LowByte = 0x05
 
+    # Define Initiation Method
     def __init__(self, I2C):
         self.I2C = I2C
         self.address = 0x10
         self.WriteRegsiter(self.CommandRegister, 0x0000)
 
+    # Create Method to Write to the Sensors Registers
     def WriteRegister(self, register, payload):
         data = bytearray([(payload & 0xFF), ((payload >> 8) & 0xFF)])
         self.I2C.writeto_mem(self.address, register, data)
 
+    # Create Method to Read from the Sensors Registers
     def ReadRegister(self, register, length=2):
-        self.I2C.readfrom_mem(self.address, register, length)
+        data = self.I2C.readfrom_mem(self.address, register, length)
         return int.from_bytes(data, "little")
 
+    # Create Method to Read the Light Data from the Sensor
     def ReadData(self):
         LowData = ReadRegister(self.LowByte, 1)
         HighData = ReadRegister(self.HighByte, 1)
@@ -36,9 +44,11 @@ class LightSensor:  #
         lux = CombinedData * self.ConversionFactor
         return lux
 
+    # Create Method to Change Sensor Configuration
     def configure(self, settings):
         self.WriteRegister(self.CommandRegister, settings)
 
+    # Create Methods to Power Up and Down
     def PowerDown(self):
         self.WriteRegister(self.CommandRegister, 0x01)
 
