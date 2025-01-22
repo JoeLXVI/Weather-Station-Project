edb843409eac51459910999dedf92c21e27b89c8
RVotta4 <robbievotta27@gmail.com>
2025-01-22 12:41:06 +0000

    First Commit

--- WeatherStation_LightSensor_V1.py
+++ WeatherStation_LightSensor_V1.py
@@ -5,3 +5,5 @@ Date Created: 26/11/2024
 Last Modified: 
 Description: This code defines a class for a light sensor including attributes and methods to allow for the reading and display of sensor data.
 """
+
+print("hello world")
--- WeatherStation_PressureSensor_v1.py
+++ WeatherStation_PressureSensor_v1.py
@@ -1,6 +1,6 @@
 """ 
 File Name: WeatherStation_PressureSensor_V1.py
-Author: Sam Young (SY947)
+Author: Sam (Cold Palmer) Young (SY947) 
 Date Created: 26/11/2024
 Last Modified: 
 Description: This code defines a class for a temperature sensor including attributes and methods to allow for the reading and display of sensor data.
