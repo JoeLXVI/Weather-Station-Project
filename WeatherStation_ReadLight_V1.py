e78f340c10d26625ae8a032b47b5cd84863427d4
JoeLXVI <joe.matthew.lucas@gmail.com>
2025-01-24 13:51:10 +0000

    Update docstring

--- WeatherStation_ReadLight_V1.py
+++ WeatherStation_ReadLight_V1.py
@@ -3,7 +3,7 @@ File Name: WeatherStation_ReadLight_V1.py
 Author: Robbie Votta (RV434)
 Date Created: 22/01/2025
 Last Modified:  22/01/2025
-Description: This code defines a class for a light sensor including attributes and methods to allow for the reading and display of sensor data.
+Description: This code reads data from a VEML6031 sensor using the machine library and a created class
 """
 
 import WeatherStation_LightSensor_V1 as LS
