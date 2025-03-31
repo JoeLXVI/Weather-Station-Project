fb2d6cb98990757955b5ae2dcb53eebbaafa530c
milesgrainger <milesgrainger17@icloud.com>
2025-03-31 13:40:21 +0100

    Add comments

--- WeatherStation_HumiditySensor_v1.py
+++ WeatherStation_HumiditySensor_v1.py
@@ -26,19 +26,20 @@ class HumiditySensor:
         raw_data = self.I2C.read_from_mem(self.address, self.RegistersDictionary["HumidityData"],2)
         return int.from_bytes(raw_data, 'little')
     
+    # Converts raw humidity data to percentage
     def read_humidity(self):
         raw_data = self.ReadData()
-        return (raw_data/16.0) #formula from datasheet
+        return (raw_data/16.0) # Formula from datasheet
 
     # Create Methods to Power Up and Down
     def PowerDown(self):
        ReadRegister = self.I2C.readfrom_mem(self.address,self.RegistersDictionary["Command"], 1)[0]
-       ReadRegister |=0x80 #set bit 7 (Power Definition bit) to 1 (power up)
+       ReadRegister |=0x80 # Set bit 7 (Power Definition bit) to 1 (power up)
        self.I2C.writeto_mem(self.address,self.RegistersDictionary["Command"], bytearray([ReadRegister]))
 
     def PowerUp(self):
        ReadRegister = self.I2C.readfrom_mem(self.address,self.RegistersDictionary["Command"], 1)[0]
-       ReadRegister |=0x80 #set bit 7 (Power Definition bit) to 1 (power up)
+       ReadRegister |=0x80 # Set bit 7 (Power Definition bit) to 1 (power up)
        self.I2C.writeto_mem(self.address,self.RegistersDictionary["Command"], bytearray([ReadRegister]))
 
     # Create Status Check
--- WeatherStation_ReadHumidity_V1.py
+++ WeatherStation_ReadHumidity_V1.py
@@ -13,6 +13,7 @@ import WeatherStation_HumiditySensor_v1 as HS
 # Innitialise I2C communication
 I2C = machine.I2C(0, machine.pin(5), machine.pin(4))
 HSObject = HS.HumiditySensor(I2C)
+# Continuously read humidity sensor and print data
 while HSObject.StatusCheck():
     time.sleep(1)
     HumidityData = HS.read_humidity()
